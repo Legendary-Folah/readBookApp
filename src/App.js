e0f5249c6ed6dcc4d81959543d0d3f755f65b263
Mo <folarinfadeyi@gmail.com>
2023-09-04 13:39:54 -0700

    hello there

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import BookList from "./Components/BookList";
+import ThemeContextProvider from "./Contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
+    <ThemeContextProvider>
       <NavBar />
       <BookList />
+    </ThemeContextProvider>
     </div>
   );
 }
