b16819de735f53b4f5acd2aef5cd1d114c227b5d
Mo <folarinfadeyi@gmail.com>
2023-09-04 11:49:20 -0700

    hello there

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
+import BookList from "./Components/BookList";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <BookList />
     </div>
   );
 }
