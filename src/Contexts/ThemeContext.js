7c624ca0f8bda7fec1123d37602d81ff490060a3
Mo <folarinfadeyi@gmail.com>
2023-09-05 23:05:47 -0700

    hello there

--- src/Contexts/ThemeContext.js
+++ src/Contexts/ThemeContext.js
@@ -8,6 +8,9 @@ class ThemeContextProvider extends Component {
         light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
         dark: {syntax: '#ddd', ui: '#333', bg: '#555'}
      } 
+     toggleTheme = () => {
+        this.setState({ isLightTheme: !this.state.isLightTheme })
+     }
     render() { 
         return (
             <ThemeContext.Provider value={{...this.state}}>
