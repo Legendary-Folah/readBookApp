f90df4ee8e59cd16da50c5db2728d4672caf2fe0
Mo <folarinfadeyi@gmail.com>
2023-09-05 22:51:48 -0700

    hello there

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,26 +4,27 @@ import { ThemeContext } from '../Contexts/ThemeContext';
 class NavBar extends Component {
   render() {
     return (
-        <ThemeContext.Consumer>{(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return(
-            <nav style={{ 
-              background: theme.ui,
-              color: theme.syntax
-             }}>
-                <h1>READING BOOK LIST</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>List</li>
-                </ul>
-            </nav>
+        <ThemeContext.Consumer>
+          {(context) => {
+            const { isLightTheme, light, dark } = context;
+            const theme = isLightTheme ? light : dark;
+            return (
+              <nav style={{ 
+                background: theme.ui,
+                color: theme.syntax
+               }}>
+                  <h1>READING BOOK LIST</h1>
+                  <ul>
+                      <li>Home</li>
+                      <li>About</li>
+                      <li>List</li>
+                  </ul>
+              </nav>
             )
-          }};
+          }}
         </ThemeContext.Consumer>
-    );
+    )
   }
-}
+};
 
 export default NavBar;
