e399e24d7ee510924ae1199ffd023562d9f100ca
Mo <folarinfadeyi@gmail.com>
2023-09-05 21:05:44 -0700

    hello there

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../Contexts/ThemeContext';
 
 class BookList extends Component {
-
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
     return (
       <div className='book-list'>
         <ul>
