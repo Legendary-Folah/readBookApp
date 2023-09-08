789da0faa5570c9cbcf70a78df21298ba450cc0a
Mo <folarinfadeyi@gmail.com>
2023-09-08 08:10:28 -0700

    hello there

---
+++ src/Components/Slider.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Slider extends Component {
+  render() {
+    return (
+      <div class="theme-switch-container">
+        <label class="theme-slider" for="checkbox">
+          <input type="checkbox" id="checkbox" />
+          <div class="round slider"></div>
+        </label>
+      </div>
+    )
+  }
+}
+
+export default Slider;
