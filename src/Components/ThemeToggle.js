48698ed955f0abc126ecdc9a3cc6fa4fa700161f
Mo <folarinfadeyi@gmail.com>
2023-09-08 08:12:38 -0700

    hello there

--- src/Components/ThemeToggle.js
+++ src/Components/ThemeToggle.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 import { ThemeContext } from "../Contexts/ThemeContext";
+import Slider from "./Slider";
 
 class ThemeToggle extends Component {
     static contextType = ThemeContext;
     render() { 
         const { toggleTheme } = this.context
         return (
-            <button onClick={toggleTheme}>Toggle</button>
+            <Slider onClick={toggleTheme}></Slider>
         );
     }
 }
