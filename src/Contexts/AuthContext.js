395be10f71f0e39fe2056c0dfe1ff88775f8ee64
Mo <folarinfadeyi@gmail.com>
2023-09-08 19:58:35 -0700

    hello there

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -1,2 +1,20 @@
-import React, { Component, createContext } from "react";
+import React, { Component, createContext, useContext } from "react";
 
+export const AuthContext = createContext();
+class AuthContextProvider extends Component {
+    state = { 
+        isAuthenticated: false
+     } 
+     toggleAuth = () => {
+        this.setState({ isAuthenticated: !this.state.isAuthenticated })
+     }
+    render() { 
+        return (
+            <AuthContextProvider.Provider value={{...this.state, toggleAuth: this.toggleAuth}}>
+                {this.props.children}
+            </AuthContextProvider.Provider>
+        );
+    }
+}
+ 
+export default AuthContextProvider;
