4d7f452f0067bfa75e9e030fdb126190f25fc0ae
Mo <folarinfadeyi@gmail.com>
2023-09-11 21:09:43 -0700

    some changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
-import NavBar from "./Components/NavBar";
-import BookList from "./Components/BookList";
-import ThemeContextProvider from "./Contexts/ThemeContext";
-import ThemeToggle from "./Components/ThemeToggle";
-import AuthContextProvider from "./Contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
-    <ThemeContextProvider>
-      <AuthContextProvider>
-      <NavBar />
-      <BookList />
-      <ThemeToggle />
-      </AuthContextProvider>
-    </ThemeContextProvider>
+      HELLO
     </div>
   );
 }
