cb7b0cfcd7be6ff4ffdeed7a7acb5fb060e4f352
Mo <folarinfadeyi@gmail.com>
2023-09-11 21:43:48 -0700

    some changes

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -3,6 +3,13 @@ import React, { createContext, useState } from 'react';
 export const BookContext = createContext();
 
 const BookContextprovider = (props) => {
+
+    const [ books, setBooks ] = useState([
+        {title: 'Man of the year', author: 'Nicolas Amiri', id: 1},
+        {title: 'Name of the Wind', author: 'Cody Anderson', id: 2},
+        {title: 'One Last Ride', author: 'Fury Micheal', id: 3}
+    ])
+
     return (
         <BookContext.Provider>
             {props.children}
