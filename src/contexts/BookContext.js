eb80c32085d530a3b160ff597dc5e2b6c5c90c58
Mo <folarinfadeyi@gmail.com>
2023-09-11 21:44:01 -0700

    some changes

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -8,7 +8,7 @@ const BookContextprovider = (props) => {
         {title: 'Man of the year', author: 'Nicolas Amiri', id: 1},
         {title: 'Name of the Wind', author: 'Cody Anderson', id: 2},
         {title: 'One Last Ride', author: 'Fury Micheal', id: 3}
-    ])
+    ]);
 
     return (
         <BookContext.Provider>
