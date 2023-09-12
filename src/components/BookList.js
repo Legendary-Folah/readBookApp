53edc02418ddc5600dd8c95f2707879c2253404e
Mo <folarinfadeyi@gmail.com>
2023-09-12 15:43:09 -0700

    change

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react'
-import { BookContext } from '../Contexts/BookContext'
+import { BookContext } from '../Contexts/BookContext';
 
 function BookList() {
     const { books } = useContext(BookContext);
-  return (
-    <div>
-
+  return books.length ? (
+    <div className='book-list'>
+        { books.map(book => {
+            return (
+                <BookDetail book={book} key={book.id} />
+            )
+        }) }
     </div>
+  ) : (
+    <div className='empty'>No books. Hello Freetime :)</div>
   )
 }
 
