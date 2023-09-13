c0db5749d53b5f9c523894b149a8e9e9fe04b3f0
Mo <folarinfadeyi@gmail.com>
2023-09-12 17:54:31 -0700

    change

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -11,7 +11,7 @@ const BookList = () => {
                 return (
                     <BookDetail book={book} key={book.id} />
                 )
-            }) }
+            })}
         </ul>
     </div>
   ) : (
