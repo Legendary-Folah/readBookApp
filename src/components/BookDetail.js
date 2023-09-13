5bd3132ddaa162e8054fde2e27bd7fef1b6a39c4
Mo <folarinfadeyi@gmail.com>
2023-09-12 19:45:19 -0700

    change

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,12 +1,21 @@
 import React, { useContext } from "react";
 import { BookContext } from "../Contexts/BookContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const BookDetail = ({book}) => {
     const { removeBook } = useContext(BookContext);
     return (
         <li onClick={() => removeBook(book.id)}>
-            <div className="title">{ book.title }</div>
-            <div className="author">{ book.author }</div>
+            <div className="title">
+                { book.title }
+            </div>
+            <div className="author">
+                { book.author }
+            </div>
+            <button>
+                <FontAwesomeIcon icon={faTrash} />
+            </button>
         </li>
     );
 }
