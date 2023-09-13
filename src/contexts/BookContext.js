df9004182e131b0310e71891c36e89752a7b17e7
Mo <folarinfadeyi@gmail.com>
2023-09-13 12:23:56 -0700

    change

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { Bookreducer } from '../Reducers/BookReducer';
 // import {v1 as uuidv1} from 'uuid';
 
@@ -8,9 +8,16 @@ const BookContextProvider = (props) => {
 
     const [ books, dispatch ] = useReducer(Bookreducer,[
         { title: 'Man of the year', author: 'Nicolas Amiri', id: 1},
-        { title: 'Name of the Wind', author: 'Cody Anderson', id: 2},
-        { title: 'One Last Ride', author: 'Fury Micheal', id: 3}
-    ]);
+        { title: 'Name of the Wind', author: 'Cody Anderson', id: 2}
+    ], () => {
+        const Data = localStorage.getItem('books');
+        return Data ? JSON.parse(Data) : [];
+    }
+    );
+
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books])
     return (
         <BookContext.Provider value={{books, dispatch}}>
             {props.children}
